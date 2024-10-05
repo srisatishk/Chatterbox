a2d056fd685d9b757a548628a09152a61e3c44ee
Zaniah Kirkpatrick <zaniahk@email.sc.edu>
2024-10-05 11:19:52 -0400

    created stub methods for #14

---
+++ language/Word.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+/**
+ * @author zaniah
+ */
+public class Word extends Question{
+    /**
+     * attributes for word class
+     */
+    private ArrayList<String> letters;
+    private String pronunciation;
+    private String translation;
+
+    /**
+     * getLetters method
+     * @return the letters in the word
+     */
+    public String getLetters(){
+        //return "get letters";
+        return null;
+    }
+
+    /**
+     * getPronunciation method
+     * @return pronunciation of the word
+     */
+    public String getPronunciation(){
+        //return "get pronunciation";
+        return null;
+    }
+
+    /**
+     * getTranslation method
+     * @return translation of the word
+     */
+    public String getTranslation(){
+        //return "get translation";
+        return null;
+    }
+}
