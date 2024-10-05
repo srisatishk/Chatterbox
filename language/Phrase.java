fc94bf9acebd8ff69a7d2000e31f803c57889c16
Zaniah Kirkpatrick <zaniahk@email.sc.edu>
2024-10-05 11:28:52 -0400

    created stub methods for #11

---
+++ language/Phrase.java
@@ -0,0 +1,49 @@
+package language;
+import java.util.ArrayList;
+/**
+ * @author zaniah
+ */
+public class Phrase extends Question {
+    /**
+     * attributes for phrase class
+     */
+    private ArrayList<Word> words;
+    private ArrayList<String> pronunciation;
+    private ArrayList<String> translation;
+    private boolean structure;
+
+    /**
+     * getWords method
+     * @return the words that will be in the phrase
+     */
+    public String getWords(){
+        //return "words";
+        return null;
+    }
+
+    /**
+     * getPronunciation method
+     * @return the pronunciation for the pharse
+     */
+    public String getPronunciation(){
+        //return "pronunciation";
+        return null;
+    }
+
+    /**
+     * getTranslation method
+     * @return the translation of the phrase in diff language
+     */
+    public String getTranslation(){
+        //return "translation";
+        return null;
+    }
+
+    /**
+     * hasStructure method
+     * @return true or false if the phrase has structure ??
+     */
+    public boolean hasStructure(){
+        return true;
+    }
+}
