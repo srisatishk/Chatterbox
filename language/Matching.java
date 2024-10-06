ac175c5899ecc2f4550e19148f18173a23e433b4
GMGustafson <gustafsg@email.sc.edu>
2024-10-06 00:19:30 -0400

    Proect #14 stub class done

---
+++ language/Matching.java
@@ -0,0 +1,31 @@
+package language;
+
+import java.util.ArrayList;
+
+public class Matching extends Question{
+
+    private ArrayList<String> imageList ;
+    private ArrayList<String> wordlst;  
+    
+
+    public String getImageList(){
+        return null;
+    }
+
+    public String getWordList()
+    {
+        return null; 
+
+    }
+    public String setImageList() 
+    {
+        return null; 
+
+    }
+    public String setWordList()
+    {
+        return null; 
+        
+    }
+} 
+
