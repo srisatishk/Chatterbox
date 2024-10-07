ef7c0225fe02beca409592d6e6e68a2fcc1e98ff
GMGustafson <gustafsg@email.sc.edu>
2024-10-07 00:39:43 -0400

    #4 and #9 stub classes made

---
+++ language/language.java
@@ -0,0 +1,31 @@
+package language;
+
+import java.util.HashMap;
+import java.util.Locale.Category;
+
+public class language {
+    
+    private String sentenceStructure; 
+    private Category currentCategory; 
+    private int progressInCategory; 
+    private HashMap<Word,Definition> translation; 
+
+  
+    public String sentenceStructure()
+    { 
+        return sentenceStructure; 
+    }
+    public void progressInLanguage()
+    { 
+        return; 
+    }
+    public void trackCategory()
+    { 
+        return; 
+    }
+    public HashMap<Word,Definition> getTranslation()
+    {
+        return null; 
+    }
+
+}
---
+++ language/languageList.java
@@ -0,0 +1,38 @@
+package language;
+
+import java.util.ArrayList;
+import java.util.Locale.Category;
+
+public class languageList {
+    
+    languageList LanguageList; 
+    private String language; 
+    private ArrayList<Category> categories; 
+    private ArrayList<Word> wordlist; 
+    private ArrayList<String> languages; 
+    private ArrayList<Phrase> phraselist; 
+
+  
+    public void LanguageList()
+    { 
+        return; 
+    }
+    public String toString()
+    { 
+        return language; 
+    }
+    public void listLanguages(languages)
+    { 
+        return; 
+    }
+    public ArrayList<String> chooseLanguage () 
+    { 
+        return languages; 
+    }
+    public ArrayList<String> getLanguage () 
+    { 
+        return languages; 
+    }
+    
+
+}
