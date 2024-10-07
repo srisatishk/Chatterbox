3935773210f6526318acce57565d5c24430af0c2
srisatishk <sriradhaas@gmail.com>
2024-10-07 07:34:33 -0400

    #3 stub class made

---
+++ language/MockConversations.java
@@ -0,0 +1,49 @@
+package language;
+/*
+ * @author sri
+ */
+import java.util.ArrayList;
+public class MockConversations {
+    /** attributes of MockConversations class */
+	private String topic;
+	private ArrayList<String> script;
+	private ArrayList<String> roles;
+	private ArrayList<String> answers;
+
+	public String getTopic(){
+	  return null;
+	}
+
+	public String getScript(){
+	  return null;
+	}
+
+	public String getRoles(){
+        return null;
+    }
+
+    public String setTopic(){
+        return null;
+    }
+
+    public String setScript(){
+        return null;
+    }
+
+    public String setRoles(ArrayList<String> roles){
+        return null;
+    }
+
+    public String getAnswers(){
+        return null;
+    }
+
+    public String getOptions(){
+        return null;
+    }
+
+    public String toString(){
+        return null;
+    }
+
+}
