e2db42fd7adf38485066b405c6f5736b52ef82e0
graciedd <graciedd@email.sc.edu>
2024-10-10 09:02:21 -0400

    updating DataConstants

--- language/DataConstants.java
+++ language/DataConstants.java
@@ -1,5 +1,17 @@
 package language;
 
-public class DataConstants {
-    
+public abstract class DataConstants {
+    protected static final String FILE_NAME_FLASHCARDS = "data.json";
+    protected static final String FILE_NAME_QUESTIONS = "question.json";
+    protected static final String FILE_NAME_PROGRESS = "progress.json";
+    protected static final String FILE_NAME_CATEGORY_SYSTEM = "categorysystem.json";
+    //Files for DataLoader - user
+    protected static final String FILE_NAME_USER = "json/User.json";
+    protected static final String USER_ID = "id";
+    protected static final String USER_FIRST_NAME = "firstName";
+    protected static final String USER_LAST_NAME = "lastName";
+    protected static final String USER_USERNAME = "username";
+    protected static final String USER_EMAIL = "email";
+    protected static final String USER_PHONE_NUMBER = "phoneNumber";
+
 }
