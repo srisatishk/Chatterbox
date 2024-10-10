c5a09b9275625c177bbd734d0e75de26a48d208b
graciedd <graciedd@email.sc.edu>
2024-10-10 02:18:27 -0400

    updating user class for the userlist #24

--- language/User.java
+++ language/User.java
@@ -34,104 +34,107 @@ public class User{
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail(){
         return email;
     }
 
-    public String getPhoneNumber(){
-        if (phoneNumber != null && phoneNumber.length() == 10) {
-            return phoneNumber.substring(0, 3) + "-" + phoneNumber.substring(3,6) + "-" + phoneNumber.substring(6);
-        }
-        return phoneNumber;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth (int year, int month, int day) {
-        if (year >= 1900 && year <= 2024 && month >= 1 && month <= 12) {
-            boolean isLeapYear = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
-            if (month == 2 ) {
-                if (day >= 1 && day <= 29 && isLeapYear) {
-                    this.dateOfBirth = LocalDate.of(year, month, day);
-                }
-                else if (day >= 1 && day <= 28 && !isLeapYear) {
-                    this.dateOfBirth = LocalDate.of(year, month, day);
-                }
-                else {
-                    this.dateOfBirth = LocalDate.of(2000, 1, 1);
-                }
-            }
-            else if (month == 4 || month == 6 || month == 9 || month == 11){
-                if (day >= 1 && day <= 30) {
-                    this.dateOfBirth = LocalDate.of(year, month, day);
-                }
-                else {
-                    this.dateOfBirth = LocalDate.of(2000, 1, 1);
-                }
-            }
-            else {
-                if (day >= 1 && day <= 30) {
-                    this.dateOfBirth = LocalDate.of(year, month, day);
-                }
-                else {
-                    this.dateOfBirth = LocalDate.of(2000, 1, 1);
-                }
-            }
-            
-        }
-        else {
-            this.dateOfBirth = LocalDate.of(2000, 1, 1);
-        }
-    }
-
-    public String getEnterUsername(){
-        return username;
-    }
-
-    public String getEnterPassword(){
-        return password;
-    }
-
-    public void forgotPassword(String newPassword){
-        String defaultPassword = "LanguageLearner123@";
-        if (newPassword.length() >= 8 && newPassword.length() <= 20){
-            System.out.print("Password must be at least 8 to 20 characters long. Now setting to the default password.");
-            this.password = defaultPassword;
-            return;
-        }
-        if (!newPassword.matches(".*[!@#$%^&()].*")) {
-            System.out.print("Password must contain at least one special character. Now setting to the default password.");
-            this.password = defaultPassword;
-            return;
-        }
-        this.password = newPassword;
+    public void setEmail(String email){
+        this.email = email;
     }
 
     public String getUsername(){
         return username;
     }
 
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getEnterUsername(){
+        return username;
+    }
+
+    public void setEnterUsername(String username){
+        this.username = username;
+    }
+
     public String getPassword(){
         return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getEnterPassword() {
+        return password;
+    }
+
+    public void setEnterPassword(String username){
+        this.username = username;
+    }
+
+    public String getPhoneNumber(){
+        if (phoneNumber != null && phoneNumber.length() == 10) {
+            return phoneNumber.substring(0, 3) + "-" + phoneNumber.substring(3, 6) + "-" + phoneNumber.substring(6);
+        }
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(int year, int month, int day) {
+        // Date of Birth logic, same as your original
+        this.dateOfBirth = calculateDateOfBirth(year, month, day);
+    }
+
+    private LocalDate calculateDateOfBirth(int year, int month, int day) {
+        // Ensure valid leap year, month, and day calculations
+        if (year >= 1900 && year <= LocalDate.now().getYear() && month >= 1 && month <= 12) {
+            boolean isLeapYear = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+            int maxDays = (month == 2 && isLeapYear) ? 29 : (month == 2) ? 28 : (month == 4 || month == 6 || month == 9 || month == 11) ? 30 : 31;
+            if (day >= 1 && day <= maxDays) {
+                return LocalDate.of(year, month, day);
+            }
+        }
+        return LocalDate.of(2000, 1, 1);  // Default value
     }
 
     public int getStreak(){
         return streak;
     }
 
-    public boolean getLogout(){
-        return true;
-    }
-
     public HashMap<language, Progress> getLanguages(){
         return languages;
     }
 
+    public void forgotPassword(String newPassword){
+        String defaultPassword = "LanguageLearner123@";
+        if (newPassword.length() <= 8 || newPassword.length() > 20 || !newPassword.matches(".*[!@#$%^&*()].*")) {
+            System.out.print("Password must be 8-20 characters and contain at least one special character. Now setting the default password.");
+            this.password = defaultPassword;
+        } else {
+            this.password = newPassword;
+        }
+    }
 }
+
 
