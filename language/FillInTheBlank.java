997653a7cbac458341d983fec61cf0c6c19b974f
srisatishk <sriradhaas@gmail.com>
2024-10-22 09:30:20 -0400

    updating files

---
+++ language/FillInTheBlank.java
@@ -0,0 +1,29 @@
+package language;
+
+import java.util.ArrayList;
+
+public class FillintheBlank {
+
+    // Attributes
+    private Phrase sampleSentence;
+    private ArrayList<String> wordBank;
+    private String userInput;
+
+
+  public String checkAnswer() 
+  {
+  }
+
+    
+  public String getMissingWord() 
+  {
+
+  }
+
+    
+  public String getSampleSentence() {
+        return sampleSentence.toString(); 
+  }
+
+     
+}
